3edd02b7cddf2b7486d319e7cb321b813ab36560
shaharx <levy.shahar13@gmail.com>
2019-05-30 02:23:24 +0300

    refactored the functions to use object instead of many variables

--- bin/alfred-deploy.js
+++ bin/alfred-deploy.js
@@ -2,20 +2,26 @@ const manager = require('../lib/manager')
 const storageManager = require('../lib/storageManager')
 const program = require('commander')
 const pkg = require('../package.json')
+const path = require('path')
 
 program
     .version(pkg.version)
     .option('-v, --artVersion <artVersion>', 'the Artifactory version to deploy')
     .option('-d, --dir [dir]', 'the path to deploy Artifactory to. current working directory by default')
     .action(() => {
-        var dir = process.cwd()
-        if (!program.artVersion) {
+        var parameters = {
+            version: program.artVersion,
+            dir: process.cwd(),
+            state: 'deploy'
+        }
+        if (!program.version) {
             console.log('Not version was specified. This is not docker and there is no default latest tag')
             return
         }
-        if (!program.dir) { console.log(`No path was specified, using ${dir} by default`) }
-        else if (program.dir[0] != '/') { dir += `/${program.dir}` }
-        storageManager.getVersion(program.artVersion, dir)
+        if (!parameters.dir) { console.log(`No path was specified, using ${parameters.dir} by default`) }
+        else if (parameters.dir[0] != '/') { parameters.dir += `/${program.dir}` }
+
+        storageManager.getVersion(parameters)
     })
 
 if (!process.argv.slice(2).length) {
