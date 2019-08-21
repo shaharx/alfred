c3ca8c228735a55c744226fa9c2e6f2ebeaf985c
shaharx <levy.shahar13@gmail.com>
2019-08-22 00:40:01 +0300

    logback remove added with out removing appenders

---
+++ bin/alfred-logback-remove.js
@@ -0,0 +1,21 @@
+const program = require('commander')
+const pkg = require('../package.json')
+// const path = require('path')
+const pathParser = require('../lib/pathParser')
+const lbmanager = require('../lib/logback-manager')
+
+program
+    .version(pkg.version)
+    .option('-n, --name <logger_name>', 'the Artifactory version to deploy')
+    .option('-p, --path [path]', 'the path to deploy Artifactory to. current working directory by default')
+    .option('-i, --index <index>', 'the logger index that will be prompted in case of logger duplicates')
+    .action(() => {
+        newPath = pathParser.parse(program.path)
+        lbmanager.removeLogger(newPath, program.name, program.index)
+    })
+
+if (!process.argv.slice(2).length) {
+    //program.outputHelp();
+}
+
+program.parse(process.argv)
--- bin/alfred-logback.js
+++ bin/alfred-logback.js
@@ -4,6 +4,7 @@ const pkg = require('../package.json')
 program
     .version(pkg.version)
     .command('add', 'add a logger to the logback.xml file')
+    .command('remove', 'remove a logger from the logback.xml file')
     .command('list', 'prints a list of all the available loggers')
     .parse(process.argv)
 
