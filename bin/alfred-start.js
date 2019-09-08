c65890ddfc7f8e9d94f7ea46afb559182d0b44fa
shaharx <levy.shahar13@gmail.com>
2019-09-08 23:36:12 +0300

    start and stop now check if the process is running against the processes and not the pid file

--- bin/alfred-start.js
+++ bin/alfred-start.js
@@ -1,27 +1,26 @@
-const fs = require('fs')
-const { exec } = require('child_process')
 const program = require('commander')
 const pkg = require('../package.json')
 const ls = require('../lib/log-system')
 const pathParser = require('../lib/pathParser')
+const im = require('../lib/instanceManager')
+const sh = require('shelljs')
+const find = require('find-process')
 
 program
     .version(pkg.version)
     .description('Start Artifactory')
     .option('-p, --path [path]', 'run the command on the current directory instead of the default server. Should be run from Artifactory home directory', '')
-    .action(() => {
+    .action(async () => {
         path = pathParser.parse(program.path)
-        const execCallback = (err, stdout, stderr) => {
-            if (err) { ls.error('ERROR:\n' + err) }
-            if (stdout) { ls.log('STDOUT:\n' + stdout) }
-            if (stderr) { ls.error('STDERR:\n' + stderr) }
-        }
-        if (!fs.existsSync(`${path}/run/artifactory.pid`)) {
-            exec(`${path}/bin/artifactory.sh start`, execCallback)
-            ls.log('Starting Artifactory')
-        } else {
-            ls.log(`Artifactory is already running. Consider stopping it first and then try again\nIf you know it is not running, remove ${path}/run/artifactory.pid and try again`)
-        }
+        var start = true
+        var processes = await find('name', 'java')
+        processes.forEach(proc => {
+            if (proc.cmd.includes(`-Dartifactory.home=${path}`)) {
+                ls.warn(`${path} is already running on procces ${proc.pid}`)
+                start = false
+            }
+        })
+        if (start) { sh.exec(path + im.getPath().art_sh + ' start') }
     })
 
 program.parse(process.argv)
--- bin/alfred-stop.js
+++ bin/alfred-stop.js
@@ -1,26 +1,26 @@
-const fs = require('fs')
-const { exec } = require('child_process')
 const program = require('commander')
 const pkg = require('../package.json')
 const ls = require('../lib/log-system')
 const pathParser = require('../lib/pathParser')
+const im = require('../lib/instanceManager')
+const sh = require('shelljs')
+const find = require('find-process')
 
 program
     .version(pkg.version)
     .description('Stop Artifactory')
     .option('-p, --path [path]', 'run the command on the current directory instead of the default server. Should be run from Artifactory home directory', '')
-    .action(() => {
+    .action(async () => {
         path = pathParser.parse(program.path)
-        const execCallback = (err, stdout, stderr) => {
-            if (err) { ls.error('ERROR:\n' + err) }
-            if (stdout) { ls.log('STDOUT:\n' + stdout) }
-            if (stderr) { ls.error('STDERR:\n' + stderr) }
-        }
-        if (fs.existsSync(`${path}/run/artifactory.pid`)) {
-            exec(`${path}/bin/artifactory.sh stop`, execCallback)
-        } else {
-            ls.log('Artifactory is not running. Consider starting it first and then try again')
-        }
+        var processes = await find('name', 'java')
+        var stop = false
+        processes.forEach(proc => {
+            if (proc.cmd.includes(`-Dartifactory.home=${path}`)) {
+                sh.exec(path + im.getPath().art_sh + ' stop')
+                stop = true
+            }
+        })
+        if(!stop){ls.warn(`${path} is not running`)}
     })
 
 program.parse(process.argv)
